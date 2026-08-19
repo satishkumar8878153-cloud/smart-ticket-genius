5a8802e4ff421d16320e42ed8f0fbdd2eb7d6023
satishkumar8878153-cloud <satishkumar8878153@gmail.com>
2026-08-19 07:56:29 +0530

    feat: dedicated My Trips screen with bottom navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+import { BottomNav } from "@/components/smart-ticket/BottomNav";
 
 function NotFoundComponent() {
   return (
@@ -126,6 +127,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <BottomNav />
     </QueryClientProvider>
   );
 }

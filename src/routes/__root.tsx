9e177e6b276a96acc1d273e08957ac2d609ffdb0
satishkumar8878153-cloud <satishkumar8878153@gmail.com>
2026-08-22 08:18:03 +0530

    feat: mount Sonner Toaster for IRCTC redirect feedback

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { BottomNav } from "@/components/smart-ticket/BottomNav";
+import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
   return (
@@ -128,6 +129,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       <Outlet />
       <BottomNav />
+      <Toaster />
     </QueryClientProvider>
   );
 }
